569ef79489f61a091c86dbdbd3fd220231453167
wsh <szymko.vlad@gmail.com>
2025-12-21 11:49:13 +0100

    add background.

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ WINDOW_HIGH = 900
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HIGH))
 pygame.display.set_caption("2d game")
 
+#Set images
+background_image = pygame.image.load("resources/Images/background.png")
+background_rect = background_image.get_rect()
+background_rect.topleft = (0,0)
+
 #The main game loop
 running = True
 while running:
@@ -17,5 +22,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+#Blit the background
+    display_surface.blit(background_image,background_rect)
+
+#Update display
+    pygame.display.update()
+
 #End the game
 pygame.quit()
